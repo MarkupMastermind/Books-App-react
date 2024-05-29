b5585fb2d39903f7523732b7a6555fb5a47046cc
MarkupMastermind <amritsamra424@gmail.com>
2024-05-28 19:24:34 -0700

    Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import AddBook from './components/AddBook';
+import SearchBook from './components/SearchBook';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-       <AddBook/>
-    </div>
+  <BrowserRouter>
+    <Routes>
+      <Route element={<AddBook/>} path='/'/>
+      <Route element={<SearchBook/>} path='/search'/>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
